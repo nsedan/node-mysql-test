9275c7ba22ac49ca0032520428ab8c3e5fb939da
nsedan <naim.alejandro.sedan@gmail.com>
2021-05-24 15:18:07 +0100

    create react app

--- packages/backend/index.js
+++ packages/backend/index.js
@@ -3,10 +3,10 @@ const mysql = require('mysql2');
 
 const dotenv = require('dotenv');
 dotenv.config();
+const PORT = process.env.PORT;
 
 const app = express();
 
-const PORT = process.env.PORT;
 
 // Connection
 const db = mysql.createConnection({
@@ -27,120 +27,9 @@ db.connect((err) => {
             message: err.message || "Not found."
         });
     }
-    console.log("MySQL connected");
+    console.log('===================================');
+    console.log('DB connection sucessful.');
 });
-
-/*
-// Create DB
-app.get('/createdb', (req, res) => {
-    let sql = 'CREATE DATABASE nodemysql';
-    db.query(sql, (err, res) => {
-        if (err) {
-            throw err;
-        }
-        console.log(res)
-    })
-})
-
-// Create TABLE
-app.get('/createtable', (req, res) => {
-    let sql = 'CREATE TABLE posts(id int AUTO_INCREMENT, title VARCHAR(255), body VARCHAR(255), PRIMARY KEY (id))';
-    db.query(sql, (err, result) => {
-        if (err) {
-            throw err;
-        }
-        console.log(result)
-        res.send('Table created')
-    })
-})
-
-// Insert post 1
-app.get('/addpost1', (req, res) => {
-    let post = {
-        title: 'post one',
-        body: 'This is post number one'
-    }
-    let sql = 'INSERT INTO posts SET ?'
-    db.query(sql, post, (err, result) => {
-        if (err) {
-            throw err;
-        }
-        console.log(result)
-        res.send('post 1 added...')
-    })
-})
-
-// Insert post 2
-app.get('/addpost2', (req, res) => {
-    let post = {
-        title: 'post two',
-        body: 'This is post number two'
-    }
-    let sql = 'INSERT INTO posts SET ?'
-    db.query(sql, post, (err, result) => {
-        if (err) {
-            throw err;
-        }
-        console.log(result)
-        res.send('post 2 added...')
-    })
-})
-
-// Fetch all posts
-app.get('/getposts', (req, res) => {
-    let sql = 'SELECT * FROM posts';
-    db.query(sql, function (err, results,) {
-        if (err) {
-            throw err;
-        }
-        console.log(results);
-        res.send('posts fetched')
-    });
-})
-
-// Fetch single posts
-app.get('/getpost/:id', (req, res) => {
-    let sql = `SELECT * FROM posts WHERE id = ${
-        req.params.id
-    }`;
-    db.query(sql, function (err, result,) {
-        if (err) {
-            throw err;
-        }
-        console.log(result);
-        res.send('post fetched')
-    });
-})
-
-// Update post
-app.get('/updatepost/:id', (req, res) => {
-    let newTitle = 'Updated title';
-    let sql = `UPDATE posts SET title = '${newTitle}' WHERE id = ${
-        req.params.id
-    }`
-    db.query(sql, function (err, result,) {
-        if (err) {
-            throw err;
-        }
-        console.log(result);
-        res.send('Post updated')
-    });
-})
-
-// Delete post
-app.get('/deletepost/:id', (req, res) => {
-    let sql = `DELETE FROM posts WHERE id = ${
-        req.params.id
-    }`
-    db.query(sql, function (err, result,) {
-        if (err) {
-            throw err;
-        }
-        console.log(result);
-        res.send('Post deleted')
-    });
-})
-*/
 
 // OPC tests
 app.get('/api/posts', (req, res) => {
@@ -255,9 +144,11 @@ app.get('/api/orders/:id', (req, res) => {
 })
 
 app.get('/', (req, res) => {
-    res.send('Server running on port 3000')
+    res.send(`Server running on port ${PORT}`)
 })
 
 app.listen(PORT, () => {
-    console.log(`serving running on port ${PORT}`);
+    console.log('===================================');
+    console.log('Server started');
+    console.log(`Listening on ${PORT}`);
 });
