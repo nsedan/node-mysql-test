630d3095e5248063a4919dcfa70fa6d1abd92515
nsedan <naim.alejandro.sedan@gmail.com>
2021-06-17 15:38:09 +0100

    formatTime function refac

--- packages/frontend/src/utils/formatTime.js
+++ packages/frontend/src/utils/formatTime.js
@@ -1,28 +1,29 @@
 const formatTime = (isoTime) => {
-    const unixtimestamp = isoTime;
-    const months_arr = [
-        "Jan",
-        "Feb",
-        "Mar",
-        "Apr",
-        "May",
-        "Jun",
-        "Jul",
-        "Aug",
-        "Sep",
-        "Oct",
-        "Nov",
-        "Dec",
-    ];
-    const date = new Date(unixtimestamp);
-    const year = date.getFullYear();
-    const month = months_arr[date.getMonth()];
-    const day = date.getDate();
-    // const hours = date.getHours();
-    // const minutes = "0" + date.getMinutes();
-    // const seconds = "0" + date.getSeconds();
-    const time = day + "-" + month + "-" + year + " " // + hours + ":" + minutes.substr(-2) + ":" + seconds.substr(-2);
-    return time;
+  const unixtimestamp = isoTime;
+  const months_arr = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  const date = new Date(unixtimestamp);
+  const year = date.getFullYear();
+  const month = months_arr[date.getMonth()];
+  const day = date.getDate();
+  // const hours = date.getHours();
+  // const minutes = "0" + date.getMinutes();
+  // const seconds = "0" + date.getSeconds();
+  // const time = `${day}-${month}-${year} ${hours}:${minutes.substr(-2)}:${seconds.substr(-2)}`;
+  const time = `${day}-${month}-${year}`;
+  return time;
 };
 
 export default formatTime;
